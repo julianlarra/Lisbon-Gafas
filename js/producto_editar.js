cd333d60d8415b6779d40f427eaaef3316289880
julianlarra <larragueta.julian@gmail.com>
2023-07-13 23:13:11 -0300

    Commit inicial

---
+++ js/producto_editar.js
@@ -0,0 +1,118 @@
+
+console.log(location.search) // lee los argumentos pasados a este formulario
+
+var id=location.search.substr(4)
+
+console.log(id)
+
+const { createApp } = Vue
+
+createApp({
+
+    data() {
+
+        return {
+
+            id:0,
+            codigo_producto:"",
+            linea:"",
+            modelo:"",
+            color:"",
+            precio:0,
+            stock:"",
+            imagen:"",
+            
+            url:'https://lisbongafas.pythonanywhere.com/productos/'  +id,
+
+        }
+
+    },
+
+    methods: {
+
+        fetchData(url) {
+
+            fetch(url)
+
+            .then(response => response.json())
+
+            .then(data => {
+
+
+                console.log(data)
+
+                this.id               = data.id
+                this.codigo_producto  = data.codigo_producto
+                this.linea            = data.linea
+                this.modelo           = data.modelo
+                this.color            = data.color
+                this.precio            = data.precio
+                this.stock            = data.stock
+                this.imagen            = data.imagen
+            }) 
+
+            .catch(err => {
+
+                console.error(err);
+
+                this.error=true
+
+            })
+
+        },
+
+        modificar() {
+
+            let producto = {
+
+                codigo_producto: this.codigo_producto,
+                linea:           this.linea,
+                modelo:          this.modelo,
+                color:           this.color,
+                precio:          this.precio,
+                stock:           this.stock,
+                imagen:          this.imagen
+
+            }
+
+            var options = {
+
+                body: JSON.stringify(producto),
+
+                method: 'PUT',
+
+                headers: { 'Content-Type': 'application/json' },
+
+                redirect: 'follow'
+
+            }
+
+            fetch(this.url, options)
+
+            .then(function () {
+
+                alert("Registro  modificado");
+
+                window.location.href = "/templates/productos.html";
+
+            })
+
+            .catch(err => {
+
+                console.error(err);
+
+                alert("Error al Modificar")
+
+            })
+
+        }
+
+    },
+
+    created() {
+
+        this.fetchData(this.url)
+
+    },
+
+}).mount('#app')
---
+++ js/productos.js
@@ -0,0 +1,137 @@
+const { createApp } = Vue
+
+createApp({
+
+    data() {
+
+        return {
+
+            productos:[],
+
+            //url:'https://localhost:5000/productos',
+            // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
+
+            url:'https://lisbongafas.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
+            error:false,
+            cargando:true,
+
+            /*atributos para el guardar los valores del formulario */
+
+            id:0,
+            codigo_producto:"",
+            linea:"",
+            modelo:"",
+            color:"",
+            precio:0,
+            stock:"",
+            imagen:"",
+            
+            
+            
+        }
+
+    },
+
+    methods: {
+
+        fetchData(url) {
+
+            fetch(url)
+
+            .then(response => response.json())
+
+            .then(data => {
+
+                this.productos = data;
+
+                this.cargando=false
+
+            })
+
+            .catch(err => {
+
+                console.error(err);
+
+                this.error=true
+
+            })
+
+        },
+
+        eliminar(producto) {
+
+            const url = this.url+'/' + producto;
+
+            var options = {
+
+                method: 'DELETE',
+
+            }
+
+            fetch(url, options)
+
+            .then(res => res.text()) // or res.json()
+
+            .then(res => {
+
+                location.reload();
+
+            })
+
+        },
+
+        grabar(){
+
+            let producto = {
+
+                codigo_producto: this.codigo_producto,
+                linea:           this.linea,
+                modelo:          this.modelo,
+                color:           this.color,
+                precio:          this.precio,
+                stock:           this.stock,
+                imagen:          this.imagen
+
+            }
+
+            var options = {
+
+                body:JSON.stringify(producto),
+
+                method: 'POST',
+
+                headers: { 'Content-Type': 'application/json' },
+
+                redirect: 'follow'
+
+            }
+
+            fetch(this.url, options)
+
+            .then(function () {
+
+                alert("Registro grabado")
+
+                window.location.href = "/templates/productos.html";
+
+            })
+
+            .catch(err => {
+
+                console.error(err);
+
+                alert("Error al Grabarr")
+
+            })
+
+        }
+
+    },
+
+    created() {
+
+        this.fetchData(this.url)
+
+    },
+
+}).mount('#app')
